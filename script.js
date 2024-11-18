8d19b52f303936a72595c30d55d1228d11640c66
BouchraBoumedien <bb891975@ens.uqam.ca>
2024-11-17 21:19:39 -0500

    changed logic and fixed errros

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 
             
             let genre = document.getElementById('genre');
-            let ageVehicule = parseInt(document.getElementsById("ageVehicule"));
+            let ageVehicule = document.getElementsById("ageVehicule");
             let valVehicule = parseFloat(document.getElementsById("val-vehicule"));
-            let nbrReclamations = parseInt(document.getElementsById("reclamation"));
-            let valReclamations = parseFloat(document.getElementsById("montantReclamations"));
-            let kilometrage = parseInt(document.getElementsById("km"));
             let camera = document.getElementsById("camera");
             let btnSoumettre = document.getElementById("btnSoumettre");
 
@@ -37,29 +34,76 @@
                 return dateActuelle.getFullYear()- dateDeNaissance.getFullYear();
             }
 
-            function calculerAgeVehicule(ageVehicule){
-                var dateAchat = new Date(dateAchat);
-                const dateActuelle = new Date();
-                var ageVehiculeActuel = dateActuelle.getFullYear()- dateAchat.getFullYear();
+
+
+            btnSoumettre.addEventListener('click', function(event){
+                let dateAchat = document.getElementById('ageVehiculeActuel').value;
+                event.preventDefault();
+                console.log(dateAchat);
+                verifierAgeVehicule(dateAchat);
+            })
+
+            function verifierAgeVehicule(dateAchat){
+                let ageVehiculeActuel = calculerAgeVehicule(dateAchat);
                 if(ageVehiculeActuel > 25){
                     document.getElementById('resultat').innerHTML = "Désolé, votre véhicule ne peut etre assuré";
                 }
+            }
+
+            function calculerAgeVehicule(dateAchat){
+                var dateAchat = new Date(dateAchat);
+                const dateActuelle = new Date();
+                return dateActuelle.getFullYear()- dateAchat.getFullYear();
+            }
+
+            
+
+            btnSoumettre.addEventListener('click', function(event){
+                let valVehicule = parseFloat(document.getElementsById("val-vehicule"));
+                event.preventDefault();
+                console.log(valVehicule);
+                verifierValeurVehicule(valVehicule);
+            })
+
+            function verifierValeurVehicule(valVehicule){
                 if(valVehicule > 100000){
                     document.getElementById('resultat').innerHTML = "Désolé, votre véhicule ne peut etre assuré";    
                 }
             }
-            
+
+
+
+            btnSoumettre.addEventListener('click', function(event){
+                let nbrReclamations = parseInt(document.getElementsById("reclamation"));
+                event.preventDefault();
+                validerNbrReclamations(nbrReclamations);
+            })
+
+
             function validerNbrReclamations(nbrReclamations){
                 if(nbrReclamations > 4){
                     document.getElementById('resultat').innerHTML = "Désolé, votre véhicule ne peut être assuré dû au nombre élevé de réclamations";
-                }    
-                        
-                if(valReclamations > 35000){
-                    document.getElementById('resultat').innerHTML = "Désolé, votre véhicule ne peut être assuré dû au montant élevé de réclamations";
-                }
+                }   
+            }
 
+            btnSoumettre.addEventListener('click', function(event){
+                let valReclamations = parseFloat(document.getElementsById("montantReclamations"));
+                event.preventDefault();
+                verifierValeurReclamation(valReclamations);
+            })
+
+            function verifierValeurReclamation(valReclamations){
+                if(valReclamations > 35000){
+                document.getElementById('resultat').innerHTML = "Désolé, votre véhicule ne peut être assuré dû au montant élevé de réclamations";
+                }
             }
             
+
+            btnSoumettre.addEventListener('click', function(event){
+                let kilometrage = parseInt(document.getElementsById("km"));
+                event.preventDefault();
+                validerKilometrage(kilometrage);
+            })
 
             function validerKilometrage(kilometrage){
                 if(kilometrage > 500000){
@@ -80,12 +124,21 @@
                 }else{
                     montantDeBase = valVehicule*0.015;
                 }
-                var assuranceAnuelle = parsefloat(document.getElementsByTagName("assurance-anuelle"));
+                return montantDeBase;
+            }
+            
+            btnSoumettre.addEventListener('click', function(event){
+                event.preventDefault();
+                calculMontantAnnuel(montantDeBase);
+            })
+
+            function calculMontantAnnuel(montantDeBase){
+                var assuranceAnnuelle = parsefloat(document.getElementsByTagName("assurance-anuelle"));
                 assuranceAnnuelle= montantDeBase +(350*nbrReclamations)+(0.02*kilometrage);
                 if(valReclamations > 25000 ){
-                    assuranceAnuelle =+ 700;
+                    assuranceAnnuelle =+ 700;
                 }
-                return assuranceAnuelle;
+                return assuranceAnnuelle;
             }
 
             function affichageResultat(){
